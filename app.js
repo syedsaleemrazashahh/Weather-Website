fb886b2c7353b5543eb90db7deaa269c5e7baec4
Syed Saleem Raza Shahh <syedsaleemrazashah69@gmail.com>
2024-05-26 13:22:25 +0500

    nothing

--- app.js
+++ app.js
@@ -3,21 +3,37 @@ const cityInput = document.querySelector("#cityInput");
 const API_KEY = "b3e90efbb5423e3395793dface9f64e6";
 const temperature = document.querySelector("#temperature");
 const humidity = document.querySelector("#humidity");
+const message = document.querySelector("#message");
+const button = document.querySelector("#Weather");
 
 const formController = async (event) => {
-  event.preventDefault();
+  try {
+    event.preventDefault();
 
-  const city = cityInput.value;
+    message.innerText = "loading...";
+    temperature.innerText = "";
+    humidity.innerText = "";
+    button.disabled = true;
 
-  const response = await fetch(
-    `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
-  );
-  const data = await response.json();
+    const city = cityInput.value;
 
-  temperature.innerText = data.main.temperature;
-  humidity.innerText = data.main.humidity;
+    const response = await axios(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    );
 
-  console.log(formController, response, data);
+    message.innerText = "";
+
+    form.reset();
+
+    temperature.innerText = `${response.data.main.temperature}°C`;
+    humidity.innerText = response.data.main.humidity;
+
+    console.log("formController, response", response.data);
+  } catch (error) {
+    console.log(error);
+
+    message.innerText = error?.response?.data?.message || "unknown error";
+  }
 };
 
 form.addEventListener("submit", formController);
